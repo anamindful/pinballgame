295db3f38be3ce4f7217e5f5d33d78a2fe7834e1
anamindful <anamindful@gmail.com>
2024-07-18 21:37:41 -0500

    rough draft of js

---
+++ pinball.js
@@ -0,0 +1,115 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 400;
+canvas.height = 400;
+
+let score = 0;
+
+class Ball {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.dx = 2;
+        this.dy = -2;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
+
+        // Collision with walls
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.draw();
+    }
+}
+
+class Paddle {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.speed = 0;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update() {
+        this.x += this.speed;
+
+        // Collision with walls
+        if (this.x < 0) {
+            this.x = 0;
+        } else if (this.x + this.width > canvas.width) {
+            this.x = canvas.width - this.width;
+        }
+
+        this.draw();
+    }
+}
+
+const ball = new Ball(canvas.width / 2, canvas.height - 30, 10, 'white');
+const paddle = new Paddle((canvas.width - 100) / 2, canvas.height - 20, 100, 10, 'white');
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+        paddle.speed = -7;
+    } else if (event.key === 'ArrowRight') {
+        paddle.speed = 7;
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+        paddle.speed = 0;
+    }
+});
+
+function detectCollision() {
+    if (ball.y + ball.radius > paddle.y && ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
+        ball.dy = -ball.dy;
+        score++;
+    }
+}
+
+function drawScore() {
+    ctx.font = '16px Arial';
+    ctx.fillStyle = 'white';
+    ctx.fillText('Score: ' + score, 8, 20);
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.update();
+    paddle.update();
+    detectCollision();
+    drawScore();
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
